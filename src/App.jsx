eeec73e7d513e6e7c6bc8bec7c97bc178c58664f
Makee <ina0312@naver.com>
2025-06-02 12:32:31 +0900

    추가 스타일링 다듬기

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Counter from "./components/Counter";
 
 export default function App() {
   return (
-    <>
-      <h1>COUNTER</h1>
+    <div className="size-full flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-extrabold tracking-wide text-center mb-6">
+        JUST A SIMPLE COUNTER
+      </h1>
       <Counter />
-    </>
+    </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,81 @@
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+// 참고: https://github.com/dcastil/tailwind-merge/blob/v3.3.0/docs/when-and-how-to-use-it.md#alternatives
+const COUNTER_BUTTON_BASE_STYLE =
+  "px-3 py-1 rounded text-white transition cursor-pointer text-sm";
+const BUTTON_VARIANTS = {
+  add: "bg-green-500 hover:bg-green-600",
+  subtract: "bg-red-500 hover:bg-red-600",
+};
+
 export default function Counter() {
+  const [countNum, setCountNum] = useState(0);
+
   return (
-    <section>
-      <div id="num-display"></div>
-      <div id="buttons"></div>
+    <section className="flex flex-col items-center gap-6 p-6">
+      <div
+        id="num-display"
+        className={twMerge(
+          "text-4xl font-bold text-stone-800 transition-colors",
+          countNum >= 5 && "text-green-500",
+          countNum >= 50 && "text-blue-500",
+          countNum >= 500 && "text-purple-500",
+          countNum <= -5 && "text-red-500",
+          countNum <= -50 && "text-orange-500",
+          countNum <= -500 && "text-yellow-500"
+        )}
+      >
+        {countNum}
+      </div>
+
+      <div id="buttons" className="grid grid-cols-3 gap-4">
+        <button
+          className={twMerge(COUNTER_BUTTON_BASE_STYLE, BUTTON_VARIANTS.add)}
+          onClick={() => setCountNum((prev) => prev + 1)}
+        >
+          +1
+        </button>
+        <button
+          className={twMerge(COUNTER_BUTTON_BASE_STYLE, BUTTON_VARIANTS.add)}
+          onClick={() => setCountNum((prev) => prev + 10)}
+        >
+          +10
+        </button>
+        <button
+          className={twMerge(COUNTER_BUTTON_BASE_STYLE, BUTTON_VARIANTS.add)}
+          onClick={() => setCountNum((prev) => prev + 100)}
+        >
+          +100
+        </button>
+        <button
+          className={twMerge(
+            COUNTER_BUTTON_BASE_STYLE,
+            BUTTON_VARIANTS.subtract
+          )}
+          onClick={() => setCountNum((prev) => prev - 1)}
+        >
+          -1
+        </button>
+        <button
+          className={twMerge(
+            COUNTER_BUTTON_BASE_STYLE,
+            BUTTON_VARIANTS.subtract
+          )}
+          onClick={() => setCountNum((prev) => prev - 10)}
+        >
+          -10
+        </button>
+        <button
+          className={twMerge(
+            COUNTER_BUTTON_BASE_STYLE,
+            BUTTON_VARIANTS.subtract
+          )}
+          onClick={() => setCountNum((prev) => prev - 100)}
+        >
+          -100
+        </button>
+      </div>
     </section>
   );
 }
